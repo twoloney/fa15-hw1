c747cc6d5f277d2140928dc857cc1bb72bcea42c
Liyang Li <liyangli@berkeley.edu>
2015-09-25 12:57:55 -0700

    submitting hw1 9/25

--- foobar.rb
+++ foobar.rb
@@ -1,3 +1,10 @@
 class Foobar
-  # Q4 CODE HERE
+  def self.baz(arr)
+  	arr.map! {|x| x.to_i}
+  	arr.map! {|x| x + 2}
+  	arr.select! {|x| x % 2 == 0}
+  	uniqueArr = arr.uniq
+  	uniqueArr.delete_if {|x| x > 10}
+  	return uniqueArr.inject {|sum, x| sum + x }
+  end
 end
--- hw1.rb
+++ hw1.rb
@@ -1,16 +1,24 @@
 def squared_sum(a, b)
-  # Q1 CODE HERE
+	#returns square of the sum of a and b
+  return (a + b) ** 2
 end
 
 def sort_array_plus_one(a)
-  # Q2 CODE HERE
+	# a = array of integers, sort a, increment every element by 1, return (destructive OK)
+  #a.sort! #directly sorts array
+  #a.sort creates a new sorted array
+  #a.map! {|x| x + 1} #map! directly modifies array
+  return a.sort.map {|x| x + 1}
 end
 
 def combine_name(first_name, last_name)
-  # Q3 CODE HERE
+  # concatenates first_name with last_name, w/ a space in between
+  return first_name + ' ' + last_name
 end
 
 def blockin_time(a)
+  # calls a class 'baz' from Foobar class
+  # write a class func in foobar.rb called "baz"
   # DO NOT EDIT THIS CODE BELOW
   require './foobar'
   Foobar.baz a
